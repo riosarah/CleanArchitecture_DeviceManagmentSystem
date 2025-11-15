53fc803be06195eec3a8fbdff548049d3e0d2368
rioSarah <you@example.coms.rio@students.htl-leonding.ac.at>
2025-11-15 16:00:36 +0100

    Added Domain Entity Validation

---
+++ Domain/ValidationSpecifications/PersonSpecifications.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValidationSpecifications
+{
+    public class PersonSpecifications
+    {
+
+        public const int NameMinLength = 2;
+
+        public static DomainValidationResult CheckFirstName(string fName) =>
+            string.IsNullOrWhiteSpace(fName)
+                ? DomainValidationResult.Failure("FirstName", "FirstName darf nicht leer sein.")
+                : DomainValidationResult.Success("FirstName");
+        public static DomainValidationResult CheckLastName(string lName) =>
+            string.IsNullOrWhiteSpace(lName)
+                ? DomainValidationResult.Failure("LastName", "LastName darf nicht leer sein.")
+                : DomainValidationResult.Success("LastName");
+
+        public static DomainValidationResult CheckMailAddress(string mailA) =>
+            string.IsNullOrWhiteSpace(mailA)
+                ? DomainValidationResult.Failure("MailAddress", "MailAddress darf nicht leer sein.")
+                : DomainValidationResult.Success("MailAddress");
+       
+    }
+}
---
+++ Domain/ValidationSpecifications/UsageSpecifications.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValidationSpecifications
+{
+    public class UsageSpecifications
+    {
+
+        public const int NameMinLength = 2;
+        public static DomainValidationResult CheckDate(DateTime to, DateTime from) =>
+            to < from
+                ? DomainValidationResult.Failure("Date", "Das Enddatum muss nach dem Startdatum liegen.")
+                : DomainValidationResult.Success("Date");
+
+        public static DomainValidationResult CheckPerson(Person person, int personid)=>
+                        person.Id != personid
+                ? DomainValidationResult.Failure("Person", "Die angegebene Person ist nicht korrekt.")
+                : DomainValidationResult.Success("Person");
+
+        public static DomainValidationResult CheckDevice( Device device, int deviceid) =>
+                        device.Id != deviceid
+                ? DomainValidationResult.Failure("Device", "Das angegebene Ger�t ist nicht korrekt.")
+                : DomainValidationResult.Success("Device"); 
+    }
+}
